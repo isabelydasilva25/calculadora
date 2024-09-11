b0d415cf46c8f18d299b752c0c7109ced83b1330
isabelydasilva25 <159267254+isabelydasilva25@users.noreply.github.com>
2024-09-11 08:30:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function calcula(opera){
-    let num1= parseFloat(document.grtElementById('zeroum').value);
+    let num1 = parseFloat(document.getElementById('zeroum').value);
+    let num2 = parseFloat(document.getElementById('zerodois').value);
+    let resultado = 0;
+    if(isNaN(num1) || isNaN(num2)){
+        alert('numero.');
+
+
+    }else{
+        if(opera == '+'){
+            resultado = num1 + num2;
+
+        }else if(opera == '-'){
+            resultado = num1 - num2;
+        
+        }else if(opera == '*'){
+            resultado = num1 * num2;
+
+        }else if(opera == '/'){
+            resultado = num1 / num2;
+        }
+        document.getElementById('resultado').textContent = 'resultado:' + resultado;
+
+    }
+
 }
+
